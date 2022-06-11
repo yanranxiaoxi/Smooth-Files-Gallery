f248be76da3821af21586e46363c61763790677e
久治明千树汐 <admin@soraharu.com>
2022-06-11 14:54:41 +0800

    feat: 0.3.0 release

--- index.php
+++ index.php
@@ -123,7 +123,7 @@ class config {
   static $__file__ = __FILE__;
   static $assets;
   static $prod = true;
-  static $version = '0.2.5';
+  static $version = '0.3.0';
   static $root;
   static $doc_root;
   static $has_login = false;
